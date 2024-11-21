03a68059f28089db12161203cfb807cbcfa6ca6d
vodavodochka <msadunov@gmail.com>
2024-11-21 20:31:44 +0500

    Done

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,9 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -14,7 +16,23 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
+        String command = String.format("ffprobe -v error -of flat -show_format %s", file.getAbsolutePath());
+        Process process = Runtime.getRuntime().exec(command);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        String out;
+        while ((out = reader.readLine()) != null) {
+            if (out.startsWith("format.tags.title")) {
+                String[] parts = out.split("=");
+                if (parts.length == 2) {
+                    return parts[1].replace("\"", "");
+                }
+            }
+        }
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new InterruptedException("Process exited with code " + exitCode);
+        }
         return "sound name";
     }
 }
